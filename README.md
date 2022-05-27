7fffde2d18dfdb6ee333d3a912c92576939d6421
shaomq <42924289+s1752729916@users.noreply.github.com>
2022-05-28 01:20:48 +0800

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # TransSfP
 Repository for paper "Transparent Shape from Single Polarization Images"
+
+## Abstract
+
+This paper presents a data-driven approach for transparent shape from polarization. Due to the inherent high transmittance, the previous shape from polarization(SfP) methods based on specular reflection model have difficulty in estimating transparent shape, and the lack of datasets for transparent SfP also limits the application of the data-driven approach. Hence, we construct the transparent SfP dataset which consists of both synthetic and real-world datasets. To determine the reliability of the physics-based reflection model, we define the physics-based prior confidence by exploiting the inherent fault of polarization information, then we propose a multi-branch fusion network to embed the confidence. Experimental results show that our approach outperforms other SfP methods. Compared with the previous method, the mean and median angular error of our approach are reduced from $19.00^\circ$ and $14.91^\circ$ to $16.72^\circ$ and $13.36^\circ$, and the accuracy $11.25^\circ, 22.5^\circ, 30^\circ$ are improved from $38.36\%, 77.36\%, 87.48\%$ to $45.51\%, 78.86\%, 89.98\%$, respectively.
+
+## Dataset
+
+<img src="https://raw.githubusercontent.com/s1752729916/githubsshaomq.github.iogithub/master/image-20220527171332530.png" alt="image-20220527171332530" style="zoom:50%;" />
+
+TransSfP dataset consists of two parts, a synthetic dataset and a real dataset.  The synthetic dataset contains 13 objects, each object has 72 sets of polarized images (different views) and a total of 936 sets. 
+The real-world data set contains 10 objects, each object contains 10-60 sets, a total of 486 sets.
+
+Download：[Baidu Netdisk (sbqn) ](https://pan.baidu.com/s/1LkuLsu_ThLxUsuI8NM22gQ ) |Google Drive
+
+
+
+## Analysis of Physics-based Prior Confidence
+
+![image-20220528001653910](https://raw.githubusercontent.com/s1752729916/githubsshaomq.github.iogithub/master/image-20220528001653910.png)
+
+The key idea of our paper is the physics-based prior confidence, which is inspired by the observation that the areas with high transmittance have more noise in the AoLP map. Here we will analyze the reason for this phenomenon in detail.
+
+First give our conclusion: **the noise in AoLP map is caused by the uncertainty of the dominance of the specular and diffuse components of the background.**
+
+The specular reflection and diffuse reflection models are often used in SfP problems to calculate the surface normals. Here we only care about the AoLP. The AoLP the specular($\psi_s$) and diffuse($\psi_d$) components are related to the azimuth$\phi$ of the surface normal as follows:
+$$
+\psi_s=\phi \pm \pi/2\\
+\psi_d=\phi \pm \pi
+$$
+The reflected light$I_t(\theta_{pol})$  of the background consists of specular$I_s(\theta_{pol})$ and diffuse$I_d(\theta_{pol})$ components:
+$$
+\begin{aligned}
+I_t(\theta_{pol}) &= I_s[1+\rho_s\cos(2\theta-2\psi_s)]+I_d[1+\rho_d\cos(2\theta-2\psi_d)] \\
+&=I_s+I_d+I_s\rho_s\cos(2\theta-2\psi_s)+I_d\rho_d\cos(2\theta-2\psi_s \mp \pi)\\
+&=I_s + I_d + |I_s\rho_s-I_d\rho_d|\cos(2\theta-2(\psi_s-\frac{1-\rm{sign}(I_s\rho_s-I_d\rho_d)}{2}\pi))\\
+&=I_{t0}[1 + \rho_t\cos(2\theta-2\psi_t)]
+\end{aligned}
+$$
+where $\theta_{pol}$ represents the angle of polarizer, $I_s, I_d$ are the average intensity of the specular and diffuse components. $\rho_s, \rho_d$ are the DoLP of the specular and diffuse components, respectively. 
+
+Therefore, the AoLP of the background reflected light $\psi_t$ can be written as:
+$$
+\begin{aligned}
+\psi_t=\psi_s-\frac{1-\rm{sign}(I_s\rho_s-I_d\rho_d)}{2}\pi
+\end{aligned}
+$$
+When the dominance of diffuse and specular components is uncertain, the signs of $I_s\rho_s-I_d\rho_d$ of adjacent points will change frequently, so the observed polarization angles of adjacent pixels will generate $\frac{ \pi}{2}$ phase shift(the well-known **$\frac{\pi}{2}$- ambiguity**), this is why the background has more noise in the AoLP map.
+
+ Since the surface of the transparent object is smooth enough, it can be assumed that the observed light of the transparent object only contains specular reflection$I_r(\theta_{pol})$ and transmission$I_t(\theta_{pol})$:
+$$
+\begin{aligned}
+I(\theta_{pol})&=I_{r0}(1-T)[1+\rho_rcos(2\theta-2\psi_r)]+I_{t0}T[1+\rho_tcos(2\theta-2\psi_t)]\\&=I_{r0}(1-T)+I_{t0}T + \sqrt{I_{r0}^2(1-T)^2\rho_r^2 + I_{t0}^2T^2\rho_t^2+2I_{r0}I_{t0}\rho_r\rho_tT(1-T)(\sin2\psi_r\sin2\psi_t+\cos2\psi_r\cos2\psi_t)}\cos(2\theta-\rm{arctan}\frac{I_{r0}(1-T)\rho_r\sin2\psi_r+I_{t0}T\rho_t\sin2\psi_t}{I_{r0}(1-T)\rho_r\cos2\psi_r+I_{t0}T\rho_t\cos2\psi_t})\\
+\end{aligned}
+$$
+$I_{r0}$ is the light intensity value corresponding to the incident light. The diffuser in the experimental setup of this paper ensures that $I_{r0}$ in all directions have the same value, $T$ is the transmission coefficient, $\rho_r, \psi_r$ are the DoLP and AoLP of the specular reflection component on the surface of the transparent object, respectively, $\rho$ and$ \psi$ are DoLP and AoLP of the transparent object surface actually observed by the camera.
+
+Rewrite the above formula into the following form:
+$$
+\begin{aligned}
+I(\theta_{pol})=I+I\rho\cos(2\theta-2\psi)
+\end{aligned}
+$$
+We can get $\psi$ as follows:
+$$
+\begin{aligned}
+\psi = \frac{1}{2}\rm{arctan}\frac{I_{r0}(1-T)\rho_r\sin2\psi_r+I_{t0}T\rho_t\sin2\psi_t}{I_{r0}(1-T)\rho_r\cos2\psi_r+I_{t0}T\rho_t\cos2\psi_t}
+\end{aligned}
+$$
+Since the value of $I_{r0}$ is determined by the light source, $I_{t0}$ is determined by the reflection of the background, usually $\frac{I_{r0}}{I_{t0}}\approx10$ , and the value of $\frac{\rho_r}{\rho_t}$ is related to the transmission coefficient $T$, but generally greater than $1$. When $T$ is small, the transmission term can be ignored , that is, $\psi=\psi_r$; when $T\rightarrow1$ , $\psi$ will be disturbed by $\psi_t$ and appear noise as shown in above Figure.
+
+## Citation
+
+If you find our work useful in your research, please consider citing:
+
+```tex
+@article{mingqi2022transparent,
+  title={Transparent Shape from Single Polarization Images},
+  author={Mingqi, Shao and Chongkun, Xia and Zhendong, Yang and Junnan, Huang and Xueqian, Wang},
+  journal={arXiv preprint arXiv:2204.06331},
+  year={2022}
+}
+```
+
---
+++ model/model.py
@@ -0,0 +1,259 @@
+import torch
+import torch.nn as nn
+import math
+
+from torch.nn.init import kaiming_normal_
+import modeling.PS_FCN.model_utils as model_utils
+import modeling.utils.SKBlock as SKBlock
+import modeling.utils.ESPABlock as ESPABlock
+import torch.nn.functional as F
+from modeling.sync_batchnorm.batchnorm import SynchronizedBatchNorm2d
+from modeling.aspp import build_aspp
+from modeling.backbone import build_backbone
+from tensorboardX import SummaryWriter
+import matplotlib.pyplot as plt
+import numpy as np
+class FinalLayer(nn.Module):
+    def __init__(self,input_channel):
+        super(FinalLayer, self).__init__()
+        ks = 3
+        pw = ks//2
+        self.conv1 = nn.Conv2d(in_channels=input_channel,out_channels=input_channel,kernel_size=ks,padding=pw)
+        self.instace_norm = nn.InstanceNorm2d(num_features=input_channel)
+        self.leaky_relu = nn.LeakyReLU()
+        self.conv2 = nn.Conv2d(in_channels=input_channel,out_channels=3,kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x1 = self.instace_norm(x1)
+        x1 = self.leaky_relu(x1)
+        x = x1 + x
+        x = self.conv2(x)
+        return x
+
+class calibrator(nn.Module):
+    def __init__(self,in_channels,out_channels,output_size):
+        super(calibrator, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = in_channels,out_channels = in_channels,kernel_size=3,padding=3//2)
+        self.leaky_relu = nn.LeakyReLU()
+        self.bn1 = nn.InstanceNorm2d(num_features=in_channels)
+
+
+        self.conv2 = nn.Conv2d(in_channels = in_channels,out_channels=out_channels,kernel_size=1)
+        self.bn2 = nn.InstanceNorm2d(num_features=out_channels)
+        self.size = output_size
+    def forward(self,x):
+        x = F.interpolate(x,size=(self.size,self.size),mode='bilinear',align_corners=False)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.leaky_relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.leaky_relu(x)
+        return x
+
+class Attention(nn.Module):
+    def __init__(self):
+        super(Attention, self).__init__()
+    def forward(self,q,k,v):
+        '''
+        Computes
+        :param q: Queries (B, C, H, W)
+        :param k: Keys (B, C, H, W)
+        :param v: Values (B, C, H, W)
+        :return:
+        '''
+        B,C,H,W = q.shape
+        H = int(H/2)
+        W = int(W/2)
+        q = F.interpolate(q,size=(H,W),mode='bilinear',align_corners=False)
+        k = F.interpolate(k,size=(H,W),mode='bilinear',align_corners=False)
+        v = F.interpolate(v,size=(H,W),mode='bilinear',align_corners=False)
+
+        q = q.view(B,1,C,H,W).view(B,1,C,H*W).permute(0,1,3,2) # (B,h,HxW,C)
+        k = k.view(B,1,C,H,W).view(B,1,C,H*W) # (B,h,C,HxW)
+        v = v.view(B,1,C,H,W).view(B,1,C,H*W).permute(0,1,3,2) # (B,h,HxW,C)
+        attn = torch.matmul(q, k) / np.sqrt(H*W) # (B,h,C,HxW)
+        attn = torch.softmax(attn,dim = -1)
+        out = torch.matmul(attn, v).permute(0, 1, 3, 2) # (B,h,C,HxW)
+        out = out.view(B,C,H,W)
+        H*=2
+        W*=2
+        out = F.interpolate(out,size=(H,W),mode='bilinear',align_corners=False)
+
+
+        return out
+
+class upSample(nn.Module):
+    def __init__(self,in_channels,out_channels,output_size):
+        super(upSample, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = in_channels*2,out_channels = in_channels,kernel_size=3,padding=3//2)
+        self.leaky_relu = nn.LeakyReLU()
+        self.bn1 = nn.InstanceNorm2d(num_features=in_channels)
+        self.conv2 = nn.Conv2d(in_channels = in_channels,out_channels=out_channels,kernel_size=1)
+        self.bn2 = nn.InstanceNorm2d(num_features=out_channels)
+        self.size = output_size
+    def forward(self,x,skip):
+        x = torch.cat((x,skip),dim = 1)
+        x = F.interpolate(x,size=(self.size,self.size),mode='bilinear',align_corners=True)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.leaky_relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.leaky_relu(x)
+
+        return x
+
+class decoder(nn.Module):
+    def __init__(self):
+        super(decoder, self).__init__()
+        self.up4 = upSample(in_channels=256,out_channels=128,output_size=64)
+        self.up3 = upSample(in_channels=128,out_channels=64,output_size=128)
+        self.up2 = upSample(in_channels=64,out_channels=32,output_size=256)
+        self.up1 = upSample(in_channels=32,out_channels=16,output_size=512)
+        self.finalLayer = FinalLayer(input_channel=16*2)
+
+    def forward(self,x,x_0,x_1,x_2,x_3,x_4):
+        # x_0: (B,16,512,512)
+        # x_1: (B,32,256,256)
+        # x_2: (B,64,128,128)
+        # x_3: (B,128,64,64)
+        # x_4: (B,256,32,32)
+
+        x = self.up4(x,x_4) #(B,128,64,64)
+        x = self.up3(x,x_3) #(B,64,128,128)
+        x = self.up2(x,x_2) #(B,32,256,256)
+        x = self.up1(x,x_1) #B(B,16,512,512)
+
+        x = torch.cat((x,x_0),dim = 1)
+        x = self.finalLayer(x) #(B,3,512,512)
+        return x
+
+
+
+class TransSfPNet(nn.Module):
+    def __init__(self, backbone='resnet', output_stride=16, num_classes=21,
+                 sync_bn=True, freeze_bn=False,device= None):
+        super(TransSfPNet, self).__init__()
+        if backbone == 'drn':
+            output_stride = 8
+
+        if sync_bn == True:
+            BatchNorm = SynchronizedBatchNorm2d
+        else:
+            BatchNorm = nn.BatchNorm2d
+        self.kernel_size = 9
+        self.lamda = 1
+        self.m = 0.5
+        self.mean_kernel = torch.ones([1,1,self.kernel_size,self.kernel_size])/self.kernel_size**2
+        self.mean_kernel = self.mean_kernel.to(device)
+        self.mean_kernel = nn.Parameter(data=self.mean_kernel, requires_grad=False)
+        self.sum_kernel_1 = torch.ones([1,1,self.kernel_size,self.kernel_size])
+        self.sum_kernel_1 = self.sum_kernel_1.to(device)
+        self.sum_kernel_1 = nn.Parameter(data=self.sum_kernel_1, requires_grad=False)
+
+        self.sum_kernel_3 = torch.ones([3,3,self.kernel_size,self.kernel_size])
+        self.sum_kernel_3 = self.sum_kernel_3.to(device)
+        self.sum_kernel_3 = nn.Parameter(data=self.sum_kernel_3, requires_grad=False)
+
+
+
+        # branch
+        self.backbone_orig = build_backbone(in_channels=2 ,backbone=backbone, output_stride=output_stride, BatchNorm=BatchNorm,Fusion=True)
+        self.aspp_orig = build_aspp(backbone, output_stride, BatchNorm)
+        self.backbone_prior = build_backbone(in_channels=12,backbone=backbone, output_stride=output_stride, BatchNorm=BatchNorm,Fusion=True)
+        self.aspp_prior = build_aspp(backbone,output_stride,BatchNorm)
+        self.backbone_atten = build_backbone(in_channels=1 ,backbone=backbone,output_stride=output_stride,BatchNorm=BatchNorm,Fusion=True)
+        self.aspp_atten = build_aspp(backbone,output_stride,BatchNorm)
+
+        self.decoder = decoder()
+
+        self.calibrator_0 = calibrator(in_channels=64,out_channels=16,output_size=512)
+        self.calibrator_1 = calibrator(in_channels=64,out_channels=32,output_size=256)
+        self.calibrator_2 = calibrator(in_channels=256,out_channels=64,output_size=128)
+        self.calibrator_3 = calibrator(in_channels=512,out_channels=128,output_size=64)
+        self.calibrator_4 = calibrator(in_channels=1024,out_channels=256,output_size=32)
+
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax()
+        if freeze_bn:
+            self.freeze_bn()
+
+        self.attention_layer = Attention()
+
+
+
+
+        self.writer = SummaryWriter()
+
+    def forward(self, orig,prior):
+        img  = orig
+        img_split = torch.split(img, 1, 1)
+        aolp = img_split[1]
+        aolp = (torch.cos(2*aolp) + 1.0)/2.0
+
+        # get attention map
+        mean_map = nn.functional.conv2d(aolp,self.mean_kernel,padding=self.kernel_size//2)
+        abs_map = torch.abs(aolp - mean_map)
+        abs_map = torch.pow(abs_map,self.m)
+        atten_map = nn.functional.conv2d(abs_map,self.sum_kernel_1,padding=self.kernel_size//2)
+        shape = atten_map.shape
+        atten_map = torch.reshape(atten_map,[shape[0],-1])
+        max_values,indices = torch.max(atten_map,dim = 1)
+        max_values = torch.reshape(max_values,[shape[0],1])
+        atten_map = torch.div(atten_map,max_values)
+        atten_map = torch.reshape(atten_map,[shape[0],shape[1],shape[2],shape[3]])
+
+
+
+        # orig-polar branch
+        x_orig, x_orig_0,x_orig_1,x_orig_2,x_orig_3,x_orig_4 = self.backbone_orig(orig)
+        x_orig = self.aspp_orig(x_orig)
+
+        # prior branch
+        x_prior, x_prior_0,x_prior_1,x_prior_2,x_prior_3,x_prior_4 = self.backbone_prior(prior)
+        x_prior = self.aspp_prior(x_prior)
+
+        # attention branch
+        x_atten,x_atten_0,x_atten_1,x_atten_2,x_atten_3,x_atten_4 = self.backbone_atten(atten_map)
+        x_atten = self.aspp_atten(x_atten)
+        x_atten = self.sigmoid(x_atten)
+        x_atten_0 = self.sigmoid(x_atten_0)
+        x_atten_1 = self.sigmoid(x_atten_1)
+        x_atten_2 = self.sigmoid(x_atten_2)
+        x_atten_3 = self.sigmoid(x_atten_3)
+        x_atten_4 = self.sigmoid(x_atten_4)
+
+        # fusion step
+        x_prior = x_prior + self.attention_layer(x_atten,x_atten,x_prior)
+
+
+        x_fusion =  x_orig + (x_atten)*x_prior
+        x_fusion_0 = x_orig_0 + (x_atten_0)*x_prior_0
+        x_fusion_1 = x_orig_1 + (x_atten_1)*x_prior_1
+        x_fusion_2 = x_orig_2 + (x_atten_2)*x_prior_2
+        x_fusion_3 = x_orig_3 + (x_atten_3)*x_prior_3
+        x_fusion_4 = x_orig_4 + (x_atten_4)*x_prior_4
+
+
+
+        x_fusion_0 = self.calibrator_0(x_fusion_0)
+        x_fusion_1 = self.calibrator_1(x_fusion_1)
+        x_fusion_2 = self.calibrator_2(x_fusion_2)
+        x_fusion_3 = self.calibrator_3(x_fusion_3)
+        x_fusion_4 = self.calibrator_4(x_fusion_4)
+
+        # decode
+        x = self.decoder(x_fusion,x_fusion_0,x_fusion_1,x_fusion_2,x_fusion_3,x_fusion_4)
+
+
+
+        return x,atten_map
+
+    def freeze_bn(self):
+        for m in self.modules():
+            if isinstance(m, SynchronizedBatchNorm2d):
+                m.eval()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.eval()
